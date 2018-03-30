3e62cb38b8c58a9b47268131b0236c4280759d45
Nick Wang <Nickwang508@gmail.com>
2018-03-30 11:59:14 -0500

    adding first files

---
+++ csv_stats_reader.py
@@ -0,0 +1,128 @@
+import pandas as pd 
+import numpy as np
+
+#creating panda data frames for player id search and salary data search
+player_register = pd.read_csv('playerid_listUTF-8.csv')
+
+salary_data = pd.read_csv('salary_data/salary_data.csv')
+
+
+
+#below are the get methods for retrieving data from the data bases
+#these will be called to retrive data in the easiest way possible
+
+#finds the players current team (STRING)
+def getPlayerTeam(playerName):
+
+	#check if player entered is in data base
+	if playerName in salary_data.Name.values:
+
+		indx = salary_data[salary_data['Name']==playerName].index.item()
+		team = salary_data.at[indx, 'Team']
+		team = team.strip()
+
+	else:
+
+		print()
+		print("Player not found")
+		return
+
+	return team
+
+#finds a players total current salary value in (INTEGER)
+def getTotalContractValue(playerName):
+
+	#check if player entered is in data base
+	if playerName in salary_data.Name.values:
+
+		indx = salary_data[salary_data['Name']==playerName].index.item()
+		salary = salary_data.at[indx, 'Salary']
+		salary = int(salary[2:].replace(",", ""))
+
+	else:
+
+		print()
+		print("Player not found")
+		return
+
+	return salary
+
+#finds num of years contract is active for (INTEGER)
+def getContractLength(playerName):
+
+	#check if player entered is in data base
+	if playerName in salary_data.Name.values:
+
+		indx = salary_data[salary_data['Name']==playerName].index.item()
+		years = salary_data.at[indx, 'Years']
+		indx_paren = years.index("(")
+		years = int(years[:indx_paren].strip())
+
+	else:
+
+		print()
+		print("Player not found")
+		return 
+
+	return years
+
+#gets year span that contract is active in (STRING)
+def getContractYears(playerName):
+	
+	#check if player entered is in data base
+	if playerName in salary_data.Name.values:
+
+		indx = salary_data[salary_data['Name']==playerName].index.item()
+		years = salary_data.at[indx, 'Years']
+		indx_paren = years.index("(")
+		indx_close = years.index(")")
+		years = years[indx_paren+1:indx_close].strip()
+
+	else:
+
+		print()
+		print("Player not found")
+		return 
+
+	return years
+
+#gets the year of signing (INTEGER)
+def getContractSignYear(playerName):
+
+	#check if player entered is in data base
+	if playerName in salary_data.Name.values:
+
+		indx = salary_data[salary_data['Name']==playerName].index.item()
+		years = salary_data.at[indx, 'Years']
+		indx_paren = years.index("(")
+		indx_dash = years.find("-")
+		year_signed = years[indx_paren+1:indx_dash]
+
+	else:
+
+		print()
+		print("Player not found")
+		return 
+
+	return year_signed
+
+#gets the players stats from the years prior to the signing of the newest contract (DATAFRAME)
+def getStatsBeforeSigning(playerName):
+
+	full_name = playerName.replace(" ", "")
+	player_stats = pd.read_csv('battingStatsPlayers/' + full_name.lower() + getPlayerTeam(playerName) + ".csv")
+
+	return player_stats
+
+def main():
+
+	user_input = input("Enter player who's salary you wish to see: ")
+	print()
+	print("Total Salary value:",getTotalContractValue(user_input))
+	print("Salary Years:",getContractYears(user_input))
+	print("Year Contract Signed:",getContractSignYear(user_input))
+	print("Contract length:",getContractLength(user_input))
+	print("Team:",getPlayerTeam(user_input))
+	print(getStatsBeforeSigning(user_input))
+
+main()
---
+++ salaryScraper.py
@@ -0,0 +1,33 @@
+import csv
+import bs4
+import re
+from urllib.request import urlopen as uReq
+from bs4 import BeautifulSoup as soup
+
+my_url = "https://www.usatoday.com/sports/mlb/salaries/"
+
+uClient = uReq(my_url)
+page_html = uClient.read()
+uClient.close()
+
+#html parsing
+page_soup = soup(page_html, "html.parser")
+
+salary_table = page_soup.find('table')
+
+headers = [header.text for header in salary_table.findAll('th')]
+
+
+rows = []
+
+#grabbing the row data
+for trs in salary_table.find_all('tr'):
+	tds = trs.find_all('td')
+	row = [elem.text.strip() for elem in tds]
+	rows.append(row)
+
+#writing the csv file
+with open('salary_data/salary_data.csv', 'w', encoding='utf8') as fp:
+    writer = csv.writer(fp)
+    writer.writerow(headers)
+    writer.writerows(row for row in rows if row)
